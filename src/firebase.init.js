068029a826953a9f6e03e0a38cd86b745e632c20
sayeef-rahman <sayeef.cse@gmail.com>
2022-04-15 08:56:51 +0600

    firebase ,react installed

---
+++ src/firebase.init.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getAuth} from 'firebase/auth';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCegYiGvCeRl8U83bqG-a0DIo3XBTYxUd0",
+  authDomain: "react-car-services-7741d.firebaseapp.com",
+  projectId: "react-car-services-7741d",
+  storageBucket: "react-car-services-7741d.appspot.com",
+  messagingSenderId: "1076330674577",
+  appId: "1:1076330674577:web:3d072519a874e5d71a8230"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+export default auth;
