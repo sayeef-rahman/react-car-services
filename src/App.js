f0a2b5a9b7c761888378981abcaf1f3ccbbd6dc2
sayeef-rahman <sayeef.cse@gmail.com>
2022-04-15 10:12:01 +0600

    reset project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import { Route, Routes } from 'react-router-dom';
+import logo from './logo.svg';
 import './App.css';
-import About from './Pages/About/About';
-import Footer from './Pages/Shared/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
-      <Footer></Footer>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
